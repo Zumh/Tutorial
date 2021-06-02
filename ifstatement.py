113756cfa3496911f44f645801a06535eced4184
Zumh <zumhliansang@live.ca>
2021-06-02 17:26:45 -0400

    ifstatement

---
+++ ifstatement.py
@@ -0,0 +1,10 @@
+
+a = 38
+o = 39
+
+if a > o:
+    print("Apple is more than Orange")
+elif a == o:
+    print("We have the same amount of Apple and Orange")
+else:
+    print("We have more Orange")
